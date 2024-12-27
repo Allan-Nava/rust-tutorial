9fb53601c3e5067c15af017850df1b7d343f02cd
allan <allan.nava@ies-italia.it>
2024-12-27 11:28:02 +0100

    Create tutorial_2 project with main function demonstrating tuple usage

---
+++ tutorial_2/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let tup : (i32, bool, char) = (1, true, 'a');
+    let tup_2 : (i8, bool, char) = (1, true, 'a');
+
+    println!("The value of tup is: {:?} tup.1: {}, tup.0: {}", tup, tup.1, tup.0);
+    println!("The value of tup_2 is: {:?} tup_2: {}, tup.0: {}", tup_2, tup_2.1, tup_2.0);
+}
