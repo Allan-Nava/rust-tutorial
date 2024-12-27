01ed1b5699cd6e1e55d5b4441ed0e70b4fe92870
allan <allan.nava@ies-italia.it>
2024-12-27 11:30:00 +0100

    Add array demonstration in main function

--- tutorial_2/src/main.rs
+++ tutorial_2/src/main.rs
@@ -4,4 +4,8 @@ fn main() {
 
     println!("The value of tup is: {:?} tup.1: {}, tup.0: {}", tup, tup.1, tup.0);
     println!("The value of tup_2 is: {:?} tup_2: {}, tup.0: {}", tup_2, tup_2.1, tup_2.0);
+
+
+    let arr: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("The value of arr is: {:?}", arr);
 }
